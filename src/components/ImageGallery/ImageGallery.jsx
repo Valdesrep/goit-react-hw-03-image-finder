172c7b5a8feee664ab052e586917ebc87aa83a5d
Vladislav Boiko <90558354+Valdesrep@users.noreply.github.com>
2022-06-09 22:53:50 +0300

    op file

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -7,3 +7,16 @@ const ImageGalery = ({}) => {
 };
 
 export default ImageGalery;
+
+const API_KEY = 26220037 - ba2b9defb736764e21d2f2b28;
+
+
+
+async function fetchImage(img, page) {
+  const params = `image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+  const response = await axios.get(
+    `${BASE_URL}?key=${API_KEY}&q=${img}&${params}`
+  );
+  const images = response.data;
+  return images;
+}
